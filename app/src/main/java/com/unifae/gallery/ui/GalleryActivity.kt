6269d5fa66ce2be0dd8bf030f059c9ac574ac539
Jaewon Gwon <jaewgwon@gmail.com>
2021-06-27 19:36:23 +0900

    Develop progress bar and hiding keyboard event

--- app/src/main/java/com/unifae/gallery/ui/GalleryActivity.kt
+++ app/src/main/java/com/unifae/gallery/ui/GalleryActivity.kt
@@ -1,7 +1,8 @@
 package com.unifae.gallery.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -27,20 +28,21 @@ class GalleryActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
         // network status observer
         viewModel.getDataSourceNetworkStatus().observe(
             this,
             Observer {
                 currentStatus = it
+                onNetworkStatusChanged(it)
             }
         )
 
         // button event handler
-        layout.galleryBtnSearch.setOnClickListener { onSearchButtonClicked() }
+        layout.galleryBtnSearch.setOnClickListener { onSearchButtonClicked(it) }
     }
 
-    private fun onSearchButtonClicked() {
+    private fun onSearchButtonClicked(view: View) {
+        hideKeyboard(view)
         if (currentStatus != NetworkStatus.LOADING) {
             viewModel
                 .searchPhotos(layout.galleryEtSearch.text.toString())
@@ -51,5 +53,15 @@ class GalleryActivity : AppCompatActivity() {
                     }
                 )
         }
+    }
+
+    private fun onNetworkStatusChanged(networkStatus: NetworkStatus) {
+        if (networkStatus == NetworkStatus.LOADING) layout.galleryWrapperLoading.visibility = View.VISIBLE
+        else if (networkStatus == NetworkStatus.LOADING_COMPLETE) layout.galleryWrapperLoading.visibility = View.GONE
+    }
+
+    fun hideKeyboard(view: View) {
+        val imm: InputMethodManager? = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
