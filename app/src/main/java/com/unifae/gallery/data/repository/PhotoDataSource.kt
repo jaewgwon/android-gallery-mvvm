295a1373bc356f5d30e01bcbaf7966c0d78648ea
Jaewon Gwon <jaewgwon@gmail.com>
2021-06-27 16:24:00 +0900

    Develop coroutine-based data source

--- app/src/main/java/com/unifae/gallery/data/repository/PhotoDataSource.kt
+++ app/src/main/java/com/unifae/gallery/data/repository/PhotoDataSource.kt
@@ -1,4 +1,38 @@
 package com.unifae.gallery.data.repository
 
-class PhotoDataSource {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.unifae.gallery.data.api.PexelsAPI
+import com.unifae.gallery.data.api.dto.PexelsPhotoData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+
+class PhotoDataSource(private val api: PexelsAPI) {
+
+    private val _networkStatus = MutableLiveData<NetworkStatus>()
+    val networkStatus: LiveData<NetworkStatus>
+        get() = _networkStatus
+
+    private val _searchResponse = MutableLiveData<List<PexelsPhotoData>>()
+    val searchResponse: LiveData<List<PexelsPhotoData>>
+        get() = _searchResponse
+
+    suspend fun fetchPhotos(query: String) =
+        withContext(Dispatchers.IO) {
+            _networkStatus.postValue(NetworkStatus.LOADING)
+            try {
+                val search = async {
+                    api.get(query)
+                }
+                _searchResponse.postValue(
+                    search.await().photos //TODO(): MAP to entity
+                )
+                _networkStatus.postValue(NetworkStatus.LOADING_COMPLETE)
+            } catch(error: Exception) {
+                Log.e("ERROR FROM PhotoDataSource", error.message.toString())
+                _networkStatus.postValue(NetworkStatus.ERROR)
+            }
+        }
 }
